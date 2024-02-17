6827ce5daedbbd551e1bbebb09f499dff5ae26d6
Silambarasan <silambarasanbabu73@gmail.com>
2024-02-17 23:06:46 +0530

    this commit for class02 of react

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "i am h1 tag"),
-    React.createElement("h2", {}, "i am h2 tag"),
+    React.createElement("h2", {}, "Silamarasan"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "i am h1 tag"),
