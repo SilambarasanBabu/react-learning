89af37e7bac7754798401aae945241de21aa2392
Silambarasan <silambarasanbabu73@gmail.com>
2024-02-22 18:10:22 +0530

    small change over here

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,20 @@
 import Card from "./Card";
 import restList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Body = () => {
   const [cd, setCd] = useState(restList);
 
+  useEffect(() => {
+    fetecData();
+  }, []);
+
+  const fetecData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = data.json();
+    console.log(json);
+  };
   return (
     <>
       <div className="search-container">
